78349981b7d1e9101527b2065c46db72b9f677ce
zehamarall <zehamarall@gmail.com>
2012-10-08 20:29:21 -0300

    Adicionando o classes para os menus do jogo

---
+++ Engine_2d/src/engine/GameOver.java
@@ -0,0 +1,64 @@
+package engine;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+
+public class GameOver extends GameCanvas {
+
+	@Override
+	public void gameUpdate(long diffTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D dbg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent tecla) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyreleassed(KeyEvent tecla) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent mouse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent mouse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent mouse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent mouse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
