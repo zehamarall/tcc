306b1c7684201eb280fb5a0e3b427385efc209ac
Leandro Amaral <Leandro Amaral@192.168.1.2>
2012-11-04 10:04:47 -0200

    Implementar classe personagem abstrata para que as classe personagem filhas tenham que implementar estes metodos

--- Engine_2d/src/engine/Personagem.java
+++ Engine_2d/src/engine/Personagem.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
-public class Personagem extends Sprite {
+public abstract class Personagem extends Sprite {
 
 	public int frame;
 	public int timeranimacao;
@@ -13,8 +13,8 @@ public class Personagem extends Sprite {
 	public int velocidade = 0;
 	public double ang = 0;
 	public boolean segueobjetivo = false;
-	public int sizeX = 24;
-	public int sizeY = 32;
+	public int sizeX;
+	public int sizeY;
 	public int velx, vely;
 	public int oldx, oldy;
 
@@ -32,37 +32,9 @@ public class Personagem extends Sprite {
 	}
 
 	@Override
-	public void simula(long diffTime) {
-		timeranimacao += diffTime;
-		frame = (timeranimacao / tempoentreframes) % 8;
-
-		x += velx * diffTime / 1000.0f;
-		y += vely * diffTime / 1000.0f;
-
-		if (velx == 0 && vely == 0) {
-			frame = 0;
-		} else if (Math.abs(velx) > Math.abs(vely)) {
-			if (velx > 0) {
-				animacao = 1;
-			} else {
-				animacao = 3;
-			}
-
-		} else {
-			if (vely > 0) {
-				animacao = 2;
-			} else {
-				animacao = 0;
-			}
-		}
-	}
+	public abstract void simula(long diffTime);
 
 	@Override
-	public void draw(Graphics2D dbg) {
-		dbg.drawImage(image, (int) x, (int) y, (int) (x + sizeX),
-				(int) (y + sizeY), sizeX * frame, sizeY * animacao,
-				(sizeX * frame) + sizeX, (sizeY * animacao) + sizeY, null);
-
-	}
+	public abstract void draw(Graphics2D dbg);
 
 }
